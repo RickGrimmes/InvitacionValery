9f1763db3f887514e05ae3a744b8d8fa8f6627d0
Ricardo Rodriguez <QuentinTaranTeemo>
2026-04-26 16:06:38 -0600

    fin

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
+      if (entry.isIntersecting) {  
         entry.target.classList.add('visible');
         observer.unobserve(entry.target);
       }
